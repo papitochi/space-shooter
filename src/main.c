9c2f4c705cb237b48b6d4638d8d02d7f18438975
papitochi <59375398+papitochi@users.noreply.github.com>
2026-01-02 19:40:52 +0200

    Fixed readme and comments

--- src/main.c
+++ src/main.c
@@ -17,10 +17,13 @@ typedef struct {
 } Projectile;
 
 int main(void) {
+
+    // Инициализация на прозореца и звуците
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
     InitAudioDevice();
     SetTargetFPS(60);
 
+    // Задаване на променливи за звуците в играта
     Sound shot = LoadSound("pew.mp3");
     Sound boom = LoadSound("gameOver.mp3");
     Sound loss = LoadSound("lifeLost.mp3");
@@ -29,7 +32,8 @@ int main(void) {
     int playerShakeX = 0;
 
     Texture2D shipTexture = LoadTexture("spaceship.png");
-
+    
+    // Героят ни
     Entity player = { {SCREEN_WIDTH/2 - 20, SCREEN_HEIGHT - 60, 40, 40}, BLUE };
     
     Projectile bullets[MAX_BULLETS] = {0};
@@ -43,21 +47,25 @@ int main(void) {
     int score = 0;
     int lives = 3;
     bool gameOver = false;
-
+    
+    // Основният цикъл на играта (проверява дали играчът е затворил прозореца или е загубил играта)
     while (!WindowShouldClose()) {
         if (!gameOver) {
+            // Поклащането на героя при загуба на живот
             if (shakeFrames > 0) {
                 playerShakeX = GetRandomValue(-5, 5);
                 shakeFrames--;
             } else {
                 playerShakeX = 0;
             }
+
+            // Контролите на играта (ляво и дясно, ограничено в екрана)
             if (IsKeyDown(KEY_RIGHT)) player.rect.x += 8.0f;
             if (IsKeyDown(KEY_LEFT)) player.rect.x -= 8.0f;
-
             if (player.rect.x < 0) player.rect.x = 0;
             if (player.rect.x > SCREEN_WIDTH - player.rect.width) player.rect.x = SCREEN_WIDTH - player.rect.width;
 
+            // Стрелба
             if (IsKeyPressed(KEY_SPACE)) {
                 PlaySound(shot);
                 for (int i = 0; i < MAX_BULLETS; i++) {
@@ -70,6 +78,7 @@ int main(void) {
                 }
             }
 
+            // Актуализиране на куршумите и враговете 
             for (int i = 0; i < MAX_BULLETS; i++) {
                 if (bullets[i].active) {
                     bullets[i].rect.y -= 10.0f;
@@ -79,12 +88,13 @@ int main(void) {
 
             for (int i = 0; i < MAX_ENEMIES; i++) {
                 enemies[i].rect.y += 4.0f;
-
+                // Ако враговете излезнат от екрана, да се покажат пак
                 if (enemies[i].rect.y > SCREEN_HEIGHT) {
                     enemies[i].rect.y = GetRandomValue(-1000, -100);
                     enemies[i].rect.x = GetRandomValue(0, SCREEN_WIDTH - 40);
                 }
 
+                // Проверка за сблъсък между героя и врага
                 if (CheckCollisionRecs(player.rect, enemies[i].rect)) {
                     if(lives>1) PlaySound(loss);
                     lives--;
@@ -99,6 +109,7 @@ int main(void) {
                 }
             }
 
+            // Проверка за сблъсък между куршума и врага
             for (int i = 0; i < MAX_BULLETS; i++) {
                 if (!bullets[i].active) continue;
                 for (int j = 0; j < MAX_ENEMIES; j++) {
@@ -111,6 +122,7 @@ int main(void) {
                 }
             }
         } else {
+            // Проверява дали е натиснат бутона 'R' за рестартиране на играта
             if (IsKeyPressed(KEY_R)) {
                 gameOver = false;
                 shakeFrames = 0;
@@ -126,10 +138,12 @@ int main(void) {
             }
         }
 
+        // Рисуване на играта
         BeginDrawing();
         ClearBackground(BLACK);
 
         if (!gameOver) {
+            // Рисуване на играча, враговете и текста
             DrawTexturePro(shipTexture, 
                            (Rectangle){0, 0, (float)shipTexture.width, (float)shipTexture.height}, 
                            (Rectangle){player.rect.x+playerShakeX,player.rect.y,player.rect.width,player.rect.height}, 
@@ -146,15 +160,16 @@ int main(void) {
             }
 
             DrawText(TextFormat("SCORE: %4d", score), 20, 20, 20, WHITE);
-            DrawText(TextFormat("LIVES: %3d", lives), 20, 40, 20, WHITE);
+            DrawText(TextFormat("LIVES: %3d", lives), SCREEN_WIDTH-125, 20, 20, WHITE);
         } else {
+            // Екран за край на играта
             DrawText("GAME OVER", SCREEN_WIDTH/2 - 125, SCREEN_HEIGHT/2 - 30, 40, WHITE);
             DrawText("Press 'R' to Restart", SCREEN_WIDTH/2 - 125, SCREEN_HEIGHT/2 + 20, 20, GRAY);
         }
-
+        // Приключване на рисуването на играта
         EndDrawing();
     }
-
+    // Освобождаване на текстурите и звуците от паметта, затваряме звуковото устройство и прозореца на играта
     UnloadTexture(shipTexture);
     UnloadSound(shot);
     UnloadSound(loss);
